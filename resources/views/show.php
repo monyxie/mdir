ba07093eefd58afe0f115d2f97d1bd7356861c90
monyxie <monyxie@gmail.com>
2020-03-19 16:20:41 +0800

    Sidebar redesign to make it look cleaner

--- resources/views/show.php
+++ resources/views/show.php
@@ -12,21 +12,25 @@
 <body>
 <div id="layout" class="pure-g">
     <div id="sidebar" class="sidebar pure-u-1 pure-u-md-1-4">
-        <div class="header">
+        <div class="side-header">
             <h1 class="brand-title"><?= $view->escape($title) ?></h1>
             <nav class="nav">
-                <?php if (!empty($ups)) { ?>
-                    <p>Go up to:</p>
                     <ul class="nav-list">
-                        <?php foreach ($ups as $directory => $path) { ?>
-                            <li class="nav-item"><a href="<?= $view->escape($path) ?>"><?= $view->escape($directory) ?></a></li>
-                            <li class="nav-item">/</li>
+                        <li class="nav-item">&raquo;</li>
+                        <li class="nav-item"><a href="<?= $view->escape('/') ?>"><?= $view->escape('Home') ?></a></li>
+                        <?php if (!empty($ups)) { ?>
+                            <?php array_shift($ups); foreach ($ups as $directory => $path) { ?>
+                                <li class="nav-item">&raquo;</li>
+                                <li class="nav-item"><a href="<?= $view->escape($path) ?>"><?= $view->escape($directory) ?></a></li>
+                            <?php } ?>
                         <?php } ?>
                     </ul>
-                <?php } ?>
+            </nav>
+        </div>
+        <div class="side-content">
+            <nav class="nav">
                 <?php if (empty($directories) && empty($files)) { ?><p>This directory is empty.</p><?php } ?>
                 <?php if (!empty($directories)) { ?>
-                    <p>Subdirectories:</p>
                     <ul class="nav-list">
                         <?php foreach ($directories as $directory => $path) { ?>
                             <li class="nav-item"><a style="background-color: <?= $path['color'] ?>" href="<?= $view->escape($path['path']) ?>"><?= $view->escape($directory) ?></a></li>
@@ -34,7 +38,6 @@
                     </ul>
                 <?php } ?>
                 <?php if (!empty($files)) { ?>
-                    <p>Files:</p>
                     <ul class="nav-list">
                         <?php foreach ($files as $file => $path) { ?>
                             <li class="nav-item"><a style="background-color: <?= $path['color'] ?>" href="<?= $view->escape($path['path']) ?>"><?= $view->escape($file) ?></a></li>
